443a0f4b90b346db12cc06f962024cd87112ab44
Samuel Belete <samuelbelete@Samuels-MacBook-Pro.local>
2019-06-02 00:09:49 -0700

    Event handlers improverd a bit

--- js/script.js
+++ js/script.js
@@ -14,6 +14,9 @@ var nameTogues = ['A','B','C','D','E','F','G','H','I','J',
                     'K','L','M','N','O','P','Q','R','S',
                     'T','U','V','W','X','Y','Z'];
 var allAlpha = document.querySelectorAll('.liBtn');
+var massageDispay1 = document.querySelector('#span1');
+var massageDispay2 = document.querySelector('#span2');
+var massageDispay3 = document.querySelector('#span3');
 
 for(var i = 0; i < allAlpha.length; i++){
     //add initial latters to add to the grid
@@ -26,11 +29,23 @@ for(var i = 0; i < allAlpha.length; i++){
        //compare the clicked letter matchs the pidked later
         if(clickedLetter === firstLetter){
             this.classList.add('ligreen');
+            this.style.fontSize = '1.5em';
             // alert('correct');
+            massageDispay1.classList.remove('span1');
+            massageDispay3.classList.add('span3')
+            massageDispay2.classList.add('span2');
+            massageDispay3.textContent = 'You are Correct!';
+            massageDispay1.textContent= '';
+
         }
         else{
             // alert('worong!');
             this.classList.add('liRed');
+            massageDispay1.classList.add('span1');
+            massageDispay3.classList.remove('span3');
+            massageDispay1.textContent = 'You are Wrong';
+            massageDispay3.textContent = ' ';
+            gi
         }
     });
 }
